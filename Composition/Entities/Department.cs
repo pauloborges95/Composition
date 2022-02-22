7a685de2c473b79e90da0829a46a8a6f416f9ac5
pauloborges95 <pauloborgesmartim@gmail.com>
2022-02-21 23:17:54 -0300

    exercicio de composicao

---
+++ Composition/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Composition.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Composition/Entities/HourContract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition.Entities
+{
+    class HourContract
+    {
+        public DateTime Date{ get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        //construtores
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        //método
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+
+       
+    }
+}
